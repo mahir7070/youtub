c0273990c4619f113e34755dd65bd46e0bd2ee24
mahir7070 <mahirraj188@gmail.com>
2025-02-12 22:54:47 +0530

    dz

--- OneDrive/Desktop/youtub/src/utils/asyinc.js
+++
@@ -1,8 +0,0 @@
-const asynchandler=(requesthandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requesthandler(req,res,next)).
-        catch((err)=>next(err))
-    }
-}
-
-export {asynchandler}
